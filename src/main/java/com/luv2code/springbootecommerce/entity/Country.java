edee03747ea081504ae7e034b4c9c4281b37e4b3
erikestr <erikestrada37@outlook.com>
2022-02-03 11:45:15 -0600

    add entity State

--- src/main/java/com/luv2code/springbootecommerce/entity/Country.java
+++ src/main/java/com/luv2code/springbootecommerce/entity/Country.java
@@ -1,4 +1,26 @@
 package com.luv2code.springbootecommerce.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    //TODO: Set up one-to-many with states
+
 }
---
+++ src/main/java/com/luv2code/springbootecommerce/entity/State.java
@@ -0,0 +1,4 @@
+package com.luv2code.springbootecommerce.entity;
+
+public class State {
+}
