14830b02bf6db6685922a098c1fc968b43727831
erikestr <erikestrada37@outlook.com>
2022-02-04 10:46:37 -0600

    update dto CheckoutServiceImpl PurchaseResponse hardcode

--- src/main/java/com/luv2code/springbootecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/luv2code/springbootecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,15 @@ package com.luv2code.springbootecommerce.service;
 import com.luv2code.springbootecommerce.dao.CustomerRepository;
 import com.luv2code.springbootecommerce.dto.Purchase;
 import com.luv2code.springbootecommerce.dto.PurchaseResponse;
+import com.luv2code.springbootecommerce.entity.Customer;
+import com.luv2code.springbootecommerce.entity.Order;
+import com.luv2code.springbootecommerce.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -17,7 +24,36 @@ public class CheckoutServiceImpl implements CheckoutService{
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+        /* retrieve the order info from dto */
+        Order order = purchase.getOrder();
+
+        /*generate tracking number */
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        /* populate order with orderItems */
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        /* populate order with billingAddress and shippingAddress */
+        order.setShippingAddress(purchase.getShippingAddress());
+        order.setBillingAddress(purchase.getBillingAddress());
+
+        /* populate customer with order */
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        /* save to the database */
+        customerRepository.save(customer);
+
+        /* return a response */
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+        /* generate a rando UUID number (UUID ver-4) */
+        return UUID.randomUUID().toString();
     }
 }
