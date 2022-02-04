0741e50e1cd0f1df39050e2ef07d953bf6491832
erikestr <erikestrada37@outlook.com>
2022-02-04 09:55:35 -0600

    update entity Order values

--- src/main/java/com/luv2code/springbootecommerce/entity/Order.java
+++ src/main/java/com/luv2code/springbootecommerce/entity/Order.java
@@ -1,4 +1,42 @@
 package com.luv2code.springbootecommerce.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name = "total_quantity")
+    private int totalQuantity;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
 }
