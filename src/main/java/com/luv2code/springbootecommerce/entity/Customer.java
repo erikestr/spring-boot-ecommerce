fc25a652027f0aab05c14ad3e348b2c8f0f76e2c
erikestr <erikestrada37@outlook.com>
2022-02-04 09:43:02 -0600

    update entity Customer values

--- src/main/java/com/luv2code/springbootecommerce/entity/Customer.java
+++ src/main/java/com/luv2code/springbootecommerce/entity/Customer.java
@@ -1,4 +1,26 @@
 package com.luv2code.springbootecommerce.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "lasta_name")
+    private String lastaName;
+
+    @Column(name = "email")
+    private String email;
 }
