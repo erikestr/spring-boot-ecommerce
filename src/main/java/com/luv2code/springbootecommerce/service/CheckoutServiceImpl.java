54e1120291bff348c6ffd56963fb5b7aa94c0dc9
erikestr <erikestrada37@outlook.com>
2022-02-04 10:29:18 -0600

    update service class CheckoutServiceImpl

--- src/main/java/com/luv2code/springbootecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/luv2code/springbootecommerce/service/CheckoutServiceImpl.java
@@ -1,4 +1,23 @@
 package com.luv2code.springbootecommerce.service;
 
-public class CheckoutServiceImpl {
+import com.luv2code.springbootecommerce.dao.CustomerRepository;
+import com.luv2code.springbootecommerce.dto.Purchase;
+import com.luv2code.springbootecommerce.dto.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+
+    /* @Autowired is optional since we only have one constructor */
+    public CheckoutServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        return null;
+    }
 }
