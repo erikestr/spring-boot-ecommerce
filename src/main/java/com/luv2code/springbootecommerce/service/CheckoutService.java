3428fac35dcb446467dd59171592d7f75d0d9db8
erikestr <erikestrada37@outlook.com>
2022-02-04 10:25:38 -0600

    update service CheckoutService Service

--- src/main/java/com/luv2code/springbootecommerce/service/CheckoutService.java
+++ src/main/java/com/luv2code/springbootecommerce/service/CheckoutService.java
@@ -1,4 +1,9 @@
 package com.luv2code.springbootecommerce.service;
 
+import com.luv2code.springbootecommerce.dto.Purchase;
+import com.luv2code.springbootecommerce.dto.PurchaseResponse;
+
 public interface CheckoutService {
+
+    PurchaseResponse placeOrder(Purchase purchase);
 }
