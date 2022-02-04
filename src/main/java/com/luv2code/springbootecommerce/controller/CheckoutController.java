c08b92917c6b47b76fec9664c0afb7e0eea372ac
erikestr <erikestrada37@outlook.com>
2022-02-04 10:51:55 -0600

    update controller CheckoutController Post PurchaseResponse

--- src/main/java/com/luv2code/springbootecommerce/controller/CheckoutController.java
+++ src/main/java/com/luv2code/springbootecommerce/controller/CheckoutController.java
@@ -1,4 +1,25 @@
 package com.luv2code.springbootecommerce.controller;
 
+import com.luv2code.springbootecommerce.dto.Purchase;
+import com.luv2code.springbootecommerce.dto.PurchaseResponse;
+import com.luv2code.springbootecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService){
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
 }
