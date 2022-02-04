2d1f44699a6b00265f932db48168e1ac3c0974cd
erikestr <erikestrada37@outlook.com>
2022-02-04 10:20:28 -0600

    update dto Purchase values

--- src/main/java/com/luv2code/springbootecommerce/dto/Purchase.java
+++ src/main/java/com/luv2code/springbootecommerce/dto/Purchase.java
@@ -1,5 +1,20 @@
 package com.luv2code.springbootecommerce.dto;
 
+import com.luv2code.springbootecommerce.entity.Address;
+import com.luv2code.springbootecommerce.entity.Customer;
+import com.luv2code.springbootecommerce.entity.Order;
+import com.luv2code.springbootecommerce.entity.OrderItem;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Purchase {
+
+    private Customer customer;
+    private Address shippingAddress;
+    private Address billingAddress;
+    private Order order;
+    private Set<OrderItem> orderItems;
 }
 
