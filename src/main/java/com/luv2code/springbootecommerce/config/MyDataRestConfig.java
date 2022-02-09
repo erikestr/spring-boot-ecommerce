6ad9aa9dda4d76dfbc37ba63f662f68a3e75cde3
Erik E <erikestrada37@outlook.com>
2022-02-08 19:10:41 -0600

    update ssl & add stripe description param on PaymentIntent

--- src/main/java/com/luv2code/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/springbootecommerce/config/MyDataRestConfig.java
@@ -49,8 +49,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     private void disableHttpMethods(Class theClass,RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
         config.getExposureConfiguration()
                 .forDomainType(theClass)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 
     private void exposeIds(RepositoryRestConfiguration config){
